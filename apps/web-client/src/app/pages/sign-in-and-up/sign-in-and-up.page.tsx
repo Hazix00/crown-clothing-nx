3fa185229b3c8830eb1dd0d767abd92cd1a64ada
@taoussi_hamza <taoussi.hamza@it.devcorp.fr>
2022-04-19 08:47:18 +0000

    previous commit

--- apps/web-client/src/app/pages/sign-in-and-up/sign-in-and-up.page.tsx
+++ apps/web-client/src/app/pages/sign-in-and-up/sign-in-and-up.page.tsx
@@ -4,12 +4,24 @@ import './sign-in-and-up.styles.scss';
 
 import SignIn from "../../components/sign-in/sign-in.component";
 import SignUp from "../../components/sign-up/sign-up.component";
+import { useSelector } from "react-redux";
+import { isUserAuthenticatedSelector } from "../../store/slices/user/auth/auth.selectors";
+import { Navigate } from "react-router-dom";
 
-const SignInAndUpPage = () => (
-    <div className="sign-in-and-sign-up">
-        <SignIn/>
-        <SignUp/>
-    </div>
-);
+const SignInAndUpPage = () => {
+
+    const authenticated = useSelector(isUserAuthenticatedSelector)
+
+    if(authenticated === true) {
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div className="sign-in-and-up">
+        <SignIn />
+        <SignUp />
+        </div>
+    ); 
+}
 
 export default SignInAndUpPage;
