2ea446a861fb060b66bf0c37fcd2d6b1c5a55ca8
@taoussi_hamza <taoussi.hamza@it.devcorp.fr>
2022-04-20 09:00:06 +0000

    fix: menuItem navigate

--- apps/web-client/src/app/components/header/header.component.tsx
+++ apps/web-client/src/app/components/header/header.component.tsx
@@ -7,6 +7,7 @@ import { AppUser } from "../../app";
 import { useDispatch, useSelector } from "react-redux";
 import { displayNameSelector, isUserAuthenticatedSelector } from "../../store/slices/user/auth/auth.selectors";
 import { logout } from "../../store/slices/user/auth/auth.slice";
+import CartIcon from "../cart/cart-icon/cart-icon";
 
 const Header: FC<AppUser> = () => {
 
@@ -33,6 +34,7 @@ const Header: FC<AppUser> = () => {
                         SIGN IN
                     </Link>
                 }
+                <CartIcon />
             </div>
         </div>
     )
--- apps/web-client/src/app/components/menu-item/menu-item.component.tsx
+++ apps/web-client/src/app/components/menu-item/menu-item.component.tsx
@@ -4,26 +4,26 @@ import { useLocation, useNavigate } from 'react-router-dom';
 import './menu-item.styles.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
-export function withRouter( Child: FC<any> ) {
-    return ( props: any ) => {
-      const location = useLocation();
-      const navigate = useNavigate();
-      return <Child { ...props } navigate={ navigate } location={ location } />;
-    }
-  }
 
-const MenuItem: FC<any> = ({title, imageUrl, size, history, linkUrl, match}) => (
-    <div className={`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
-    <div 
-    style={{
-        backgroundImage:`url(${imageUrl})`
-    }}
-    className="background-image"></div>
-        <div className="content">
-            <h1 className="title">{title.toUpperCase()}</h1>
-            <span className="subtitle">SHOP NOW</span>
+const MenuItem: FC<any> = ({title, imageUrl, size, linkUrl}) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    return (
+        <div className={`${size} menu-item`} onClick={() => navigate(`${location.pathname}${linkUrl}`)}>
+        <div 
+        style={{
+            backgroundImage:`url(${imageUrl})`
+        }}
+        className="background-image"></div>
+            <div className="content">
+                <h1 className="title">{title.toUpperCase()}</h1>
+                <span className="subtitle">SHOP NOW</span>
+            </div>
         </div>
-    </div>
-)
+    )
 
-export default withRouter(MenuItem);
+}
+
+
+
+export default MenuItem;
